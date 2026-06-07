fb889078bda0cefab7864152a1f543af91451fa9
AB-stack-cmd <abirsarkar123455@gmail.com>
2026-06-07 23:51:29 +0530

    added load_dataset funtction

--- data_set.py
+++ data_set.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import pandas as pd
 from datetime import date
 from sklearn.model_selection import train_test_split
-
+import numpy as np
 
 def dataset_save(company_name:str , start:str , end = date.today()):
         """
@@ -29,18 +29,10 @@ def dataset_save(company_name:str , start:str , end = date.today()):
 def read_df(df_name):
        return
 
+
+
 data = pd.read_excel("dataset_AAPL_2010-01-01.xlsx")
 
-
-
-start_date = "2010-01-01"
-end_date = "2020-01-01"
-close = data["Close"]
-print(len(close))
-# Fetch historical stock data
-data = yf.download("AAPL", start=start_date, end=end_date)
-
-# print(data.head())
 
 def sma_50(data):
     """
@@ -51,7 +43,15 @@ def sma_50(data):
     data['SMA50'] = data['Close'].rolling(50).mean()
     return data["SMA50"]
 
-data['SMA50'] = data['Close'].rolling(50).mean()
+# data['SMA50'] = data['Close'].rolling(50).mean()
 # print(data.head())
 # print(data["SMA50"])
-print(data["SMA50"].isna().sum())
+# print(data["SMA50"].isna().sum())
+
+def load_data(csv_file):
+      df = pd.read_csv(csv_file)
+      df["Date"] = pd.to_datetime(df["Date"])
+      df.set_index(df["Date"],inplace=True)
+      
+      return df
+
